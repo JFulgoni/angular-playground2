d5e11bfbdd9a979847f50e0cda96d2e4c82a4968
John Fulgoni <jsf2154@columbia.edu>
2021-10-07 19:48:50 -0400

    Adding games table features

--- src/app/games/games.component.ts
+++ src/app/games/games.component.ts
@@ -1,5 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 import { ApiService } from '../services/api.service';
+
+export interface Game {
+  id: string;
+  title: string;
+  short_description: string;
+}
 
 @Component({
   selector: 'app-games',
@@ -8,16 +17,22 @@ import { ApiService } from '../services/api.service';
 })
 export class GamesComponent implements OnInit {
 
-  constructor(private apiService: ApiService) { }
+  displayedColumns: string[] = ['id', 'title', 'short_description'];
+  dataSource: MatTableDataSource<Game>;
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  constructor(private apiService: ApiService) {}
 
   ngOnInit(): void {
-    this.getGamesFromApi();
-  }
-
-  getGamesFromApi() {
     this.apiService.getGames().subscribe(
       gameData => {
-        console.log(gameData);
+        this.dataSource = new MatTableDataSource(gameData);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.paginator.firstPage();
+        this.dataSource.filterPredicate = (data: Game, filter: string) => {
+          return data.title.toLowerCase().includes(filter);
+        }
       },
       error => {
         console.log(error);
@@ -25,6 +40,13 @@ export class GamesComponent implements OnInit {
     );
   }
 
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
 
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
 
 }
